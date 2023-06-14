ccd1d86cd124d7cc1a3d6c93978bc60910646de6
Martyna Firgolska <mafirgolska@gmail.com>
2023-06-14 22:34:19 +0200

    fix import and trainer

--- src/deepemotions/pipelines/model_train/nodes.py
+++ src/deepemotions/pipelines/model_train/nodes.py
@@ -1,18 +1,18 @@
 import pytorch_lightning as pl
 from .data import EmotionDataModule
-from .model import EmotionClassifier
+from .model import EmotionModel
 
 def get_datamodule(df):
     return EmotionDataModule(df)
 
 def get_model(pretrained_bert):
-    return EmotionClassifier(pretrained_bert)
+    return EmotionModel(pretrained_bert)
 
 def get_trainer():
     return pl.Trainer(
-        gpus = 1,
+        # gpus = 1,
         max_epochs = 5,
-        progress_bar_refresh_rate = 30
+        # progress_bar_refresh_rate = 30
     )
 
 def train_model(model, trainer, datamodule):
