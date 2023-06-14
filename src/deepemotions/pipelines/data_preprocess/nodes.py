2641e9c82e14f5b312eef9b43693ce9908eb8371
mabuszka <magda@buszka.eu>
2023-06-14 23:17:39 +0200

    update preprocessing

--- src/deepemotions/pipelines/data_preprocess/nodes.py
+++ src/deepemotions/pipelines/data_preprocess/nodes.py
@@ -9,8 +9,21 @@ def narrow_dataset(dataset):
     ], axis = "columns")
     return narrowed_dateset
 
-def clean_dataset(narrowed_dataset):
-    clean_dataset = narrowed_dataset[~narrowed_dataset['example_very_unclear']]
-    clean_dataset = clean_dataset.drop(['example_very_unclear'], axis = "columns")
+def group_dataset(narrowed_dateset):
+    grouped_dataset = narrowed_dateset.groupby(["id", "text"])
+    return grouped_dataset 
+
+def drop_discordant_annotations(grouped_dataset):
+    filtered_dataset = grouped_dataset.sum()
+    filtered_dataset = filtered_dataset.replace(to_replace = 1, value = 0).replace(
+        to_replace = [
+            k for k in range(2, filtered_dataset.max(axis = None) + 1 )],
+              value = 1)
+    filtered_dataset = filtered_dataset.loc[~(filtered_dataset==0).all(axis=1)]
+    filtered_dataset = filtered_dataset.reset_index()
+    return filtered_dataset 
+
+def clean_dataset(filtered_dataset):
+    clean_dataset = filtered_dataset.drop(['example_very_unclear'], axis = "columns")
     return clean_dataset
 
--- src/deepemotions/pipelines/data_preprocess/pipeline.py
+++ src/deepemotions/pipelines/data_preprocess/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.5
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import narrow_dataset, clean_dataset
+from .nodes import narrow_dataset, drop_discordant_annotations, clean_dataset, group_dataset
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -15,9 +15,21 @@ def create_pipeline(**kwargs) -> Pipeline:
             name="narrow_dataset_node",
         ),
         node(
-            func=clean_dataset,
+            func=group_dataset,
             inputs="narrowed-dataset",
+            outputs="grouped-dataset",
+            name="group_dataset_node",
+        ),
+        node(
+            func=drop_discordant_annotations,
+            inputs="grouped-dataset",
+            outputs="filtered-dataset",
+            name="drop_discordant_annotations_node",
+        ),
+        node(
+            func=clean_dataset,
+            inputs="filtered-dataset",
             outputs="clean-dataset",
             name="clean_dataset_node",
-        ),
+        )
     ])
